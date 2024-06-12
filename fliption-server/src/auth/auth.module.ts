4e4fa8725c28ce29e8da562d34582c840c92c417
kim.mainsain <kmsid003@gmail.com>
2024-06-12 21:53:22 +0900

    Feat : 로그인 성공 시 토큰 반환

--- fliption-server/src/auth/auth.module.ts
+++ fliption-server/src/auth/auth.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'TEMP_SECRET_KEY',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, UserService],
   controllers: [AuthController],
 })
--- fliption-server/src/auth/auth.service.ts
+++ fliption-server/src/auth/auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginDto } from '../user/dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  signIn(loginDto: LoginDto) {
+  signIn(loginDto: LoginDto): string {
     const user = this.userService.findOne(loginDto.username);
     if (user?.password !== loginDto.password) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    return result;
+    const payload = { username: user.username, sub: user.userId };
+    return this.jwtService.sign(payload);
   }
 }
